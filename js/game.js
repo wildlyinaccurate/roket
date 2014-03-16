2d3b44fae24dbbd2e5e2c70596d55bbac0363dfb
Joseph Wynn <joseph@wildlyinaccurate.com>
2014-03-16 13:05:41 +1300

    Show altitude

--- js/game.js
+++ js/game.js
@@ -19,6 +19,7 @@ function tick() {
 
     // One day this will be a nice interface for the player
     info.innerHTML = 'Co-ordinates: [' + Math.round(roket.x) + ', ' + Math.round(roket.y) + ']<br>' +
+        'Altitude: ' + -Math.round((roket.y - canvas.height + roket.height)) + 'm<br>' +
         'Fuel: ' + roket.fuel.toFixed(2) + ' kg<br>' +
         'Thrust: ' + roket.thrust + ' N<br>' +
         'Acceleration: ' + roket.acceleration().toFixed(2) + ' m/s<br>' +
